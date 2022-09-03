050da38c5d75b3a8dd052e5aa59d0f68de0c6ae4
Flávia Bulad <flavia.bulad@gmail.com>
2022-09-03 14:26:04 -0300

    fix: eslint and ts config files

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ import cors from 'cors';
 import 'express-async-errors';
 import dotenv from 'dotenv';
 
+import router from './routes/index.js';
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors(), json());
+app.use(router);
 
 const PORT: number = Number(process.env.PORT) || 5000;
 
